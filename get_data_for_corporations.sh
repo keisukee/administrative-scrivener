68ba804bb04ea1cba55c51c0fbf6771c99169584
keisuke ohashi <kog71903@gmail.com>
2020-02-04 15:31:39 +0900

    法人に対してスクレイピングするシェルスクリプト

---
+++ get_data_for_corporations.sh
@@ -0,0 +1,96 @@
+#!/bin/zsh
+
+echo "北海道 is begin"
+ruby scrape_corporations.rb 北海道 > output/北海道法人.txt
+echo "青森県 is begin"
+ruby scrape_corporations.rb 青森県 > output/青森県法人.txt
+echo "岩手県 is begin"
+ruby scrape_corporations.rb 岩手県 > output/岩手県法人.txt
+echo "宮城県 is begin"
+ruby scrape_corporations.rb 宮城県 > output/宮城県法人.txt
+echo "秋田県 is begin"
+ruby scrape_corporations.rb 秋田県 > output/秋田県法人.txt
+echo "山形県 is begin"
+ruby scrape_corporations.rb 山形県 > output/山形県法人.txt
+echo "福島県 is begin"
+ruby scrape_corporations.rb 福島県 > output/福島県法人.txt
+echo "東京都 is begin"
+ruby scrape_corporations.rb 東京都 > output/東京都法人.txt
+echo "神奈川県 is begin"
+ruby scrape_corporations.rb 神奈川県 > output/神奈川県法人.txt
+echo "埼玉県 is begin"
+ruby scrape_corporations.rb 埼玉県 > output/埼玉県法人.txt
+echo "千葉県 is begin"
+ruby scrape_corporations.rb 千葉県 > output/千葉県法人.txt
+echo "茨城県 is begin"
+ruby scrape_corporations.rb 茨城県 > output/茨城県法人.txt
+echo "栃木県 is begin"
+ruby scrape_corporations.rb 栃木県 > output/栃木県法人.txt
+echo "群馬県 is begin"
+ruby scrape_corporations.rb 群馬県 > output/群馬県法人.txt
+echo "山梨県 is begin"
+ruby scrape_corporations.rb 山梨県 > output/山梨県法人.txt
+echo "新潟県 is begin"
+ruby scrape_corporations.rb 新潟県 > output/新潟県法人.txt
+echo "長野県 is begin"
+ruby scrape_corporations.rb 長野県 > output/長野県法人.txt
+echo "富山県 is begin"
+ruby scrape_corporations.rb 富山県 > output/富山県法人.txt
+echo "石川県 is begin"
+ruby scrape_corporations.rb 石川県 > output/石川県法人.txt
+echo "福井県 is begin"
+ruby scrape_corporations.rb 福井県 > output/福井県法人.txt
+echo "愛知県 is begin"
+ruby scrape_corporations.rb 愛知県 > output/愛知県法人.txt
+echo "岐阜県 is begin"
+ruby scrape_corporations.rb 岐阜県 > output/岐阜県法人.txt
+echo "静岡県 is begin"
+ruby scrape_corporations.rb 静岡県 > output/静岡県法人.txt
+echo "三重県 is begin"
+ruby scrape_corporations.rb 三重県 > output/三重県法人.txt
+echo "大阪府 is begin"
+ruby scrape_corporations.rb 大阪府 > output/大阪府法人.txt
+echo "兵庫県 is begin"
+ruby scrape_corporations.rb 兵庫県 > output/兵庫県法人.txt
+echo "京都府 is begin"
+ruby scrape_corporations.rb 京都府 > output/京都府法人.txt
+echo "滋賀県 is begin"
+ruby scrape_corporations.rb 滋賀県 > output/滋賀県法人.txt
+echo "奈良県 is begin"
+ruby scrape_corporations.rb 奈良県 > output/奈良県法人.txt
+echo "和歌山県 is begin"
+ruby scrape_corporations.rb 和歌山県 > output/和歌山県法人.txt
+echo "鳥取県 is begin"
+ruby scrape_corporations.rb 鳥取県 > output/鳥取県法人.txt
+echo "島根県 is begin"
+ruby scrape_corporations.rb 島根県 > output/島根県法人.txt
+echo "岡山県 is begin"
+ruby scrape_corporations.rb 岡山県 > output/岡山県法人.txt
+echo "広島県 is begin"
+ruby scrape_corporations.rb 広島県 > output/広島県法人.txt
+echo "山口県 is begin"
+ruby scrape_corporations.rb 山口県 > output/山口県法人.txt
+echo "徳島県 is begin"
+ruby scrape_corporations.rb 徳島県 > output/徳島県法人.txt
+echo "香川県 is begin"
+ruby scrape_corporations.rb 香川県 > output/香川県法人.txt
+echo "愛媛県 is begin"
+ruby scrape_corporations.rb 愛媛県 > output/愛媛県法人.txt
+echo "高知県 is begin"
+ruby scrape_corporations.rb 高知県 > output/高知県法人.txt
+echo "福岡県 is begin"
+ruby scrape_corporations.rb 福岡県 > output/福岡県法人.txt
+echo "佐賀県 is begin"
+ruby scrape_corporations.rb 佐賀県 > output/佐賀県法人.txt
+echo "長崎県 is begin"
+ruby scrape_corporations.rb 長崎県 > output/長崎県法人.txt
+echo "熊本県 is begin"
+ruby scrape_corporations.rb 熊本県 > output/熊本県法人.txt
+echo "大分県 is begin"
+ruby scrape_corporations.rb 大分県 > output/大分県法人.txt
+echo "宮崎県 is begin"
+ruby scrape_corporations.rb 宮崎県 > output/宮崎県法人.txt
+echo "鹿児島県 is begin"
+ruby scrape_corporations.rb 鹿児島県 > output/鹿児島県法人.txt
+echo "沖縄県 is begin"
+ruby scrape_corporations.rb 沖縄県 > output/沖縄県法人.txt
