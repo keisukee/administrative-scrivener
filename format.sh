5ec973be5b6c666d0213a36291fc522bd715718f
keisuke ohashi <kog71903@gmail.com>
2020-02-04 15:03:43 +0900

    フォーマットを整えるスクリプト作成

---
+++ format.sh
@@ -0,0 +1,96 @@
+#!/bin/zsh
+
+ruby format.rb output/北海道.txt formatted/北海道.xls
+echo "北海道.txt formatted/北海道.xls"
+ruby format.rb output/青森県.txt formatted/青森県.xls
+echo "青森県.txt formatted/青森県.xls"
+ruby format.rb output/岩手県.txt formatted/岩手県.xls
+echo "岩手県.txt formatted/岩手県.xls"
+ruby format.rb output/宮城県.txt formatted/宮城県.xls
+echo "宮城県.txt formatted/宮城県.xls"
+ruby format.rb output/秋田県.txt formatted/秋田県.xls
+echo "秋田県.txt formatted/秋田県.xls"
+ruby format.rb output/山形県.txt formatted/山形県.xls
+echo "山形県.txt formatted/山形県.xls"
+ruby format.rb output/福島県.txt formatted/福島県.xls
+echo "福島県.txt formatted/福島県.xls"
+ruby format.rb output/東京都.txt formatted/東京都.xls
+echo "東京都.txt formatted/東京都.xls"
+ruby format.rb output/神奈川県.txt formatted/神奈川県.xls
+echo "神奈川県.txt formatted/神奈川県.xls"
+ruby format.rb output/埼玉県.txt formatted/埼玉県.xls
+echo "埼玉県.txt formatted/埼玉県.xls"
+ruby format.rb output/千葉県.txt formatted/千葉県.xls
+echo "千葉県.txt formatted/千葉県.xls"
+ruby format.rb output/茨城県.txt formatted/茨城県.xls
+echo "茨城県.txt formatted/茨城県.xls"
+ruby format.rb output/栃木県.txt formatted/栃木県.xls
+echo "栃木県.txt formatted/栃木県.xls"
+ruby format.rb output/群馬県.txt formatted/群馬県.xls
+echo "群馬県.txt formatted/群馬県.xls"
+ruby format.rb output/山梨県.txt formatted/山梨県.xls
+echo "山梨県.txt formatted/山梨県.xls"
+ruby format.rb output/新潟県.txt formatted/新潟県.xls
+echo "新潟県.txt formatted/新潟県.xls"
+ruby format.rb output/長野県.txt formatted/長野県.xls
+echo "長野県.txt formatted/長野県.xls"
+ruby format.rb output/富山県.txt formatted/富山県.xls
+echo "富山県.txt formatted/富山県.xls"
+ruby format.rb output/石川県.txt formatted/石川県.xls
+echo "石川県.txt formatted/石川県.xls"
+ruby format.rb output/福井県.txt formatted/福井県.xls
+echo "福井県.txt formatted/福井県.xls"
+ruby format.rb output/愛知県.txt formatted/愛知県.xls
+echo "愛知県.txt formatted/愛知県.xls"
+ruby format.rb output/岐阜県.txt formatted/岐阜県.xls
+echo "岐阜県.txt formatted/岐阜県.xls"
+ruby format.rb output/静岡県.txt formatted/静岡県.xls
+echo "静岡県.txt formatted/静岡県.xls"
+ruby format.rb output/三重県.txt formatted/三重県.xls
+echo "三重県.txt formatted/三重県.xls"
+ruby format.rb output/大阪府.txt formatted/大阪府.xls
+echo "大阪府.txt formatted/大阪府.xls"
+ruby format.rb output/兵庫県.txt formatted/兵庫県.xls
+echo "兵庫県.txt formatted/兵庫県.xls"
+ruby format.rb output/京都府.txt formatted/京都府.xls
+echo "京都府.txt formatted/京都府.xls"
+ruby format.rb output/滋賀県.txt formatted/滋賀県.xls
+echo "滋賀県.txt formatted/滋賀県.xls"
+ruby format.rb output/奈良県.txt formatted/奈良県.xls
+echo "奈良県.txt formatted/奈良県.xls"
+ruby format.rb output/和歌山県.txt formatted/和歌山県.xls
+echo "和歌山県.txt formatted/和歌山県.xls"
+ruby format.rb output/鳥取県.txt formatted/鳥取県.xls
+echo "鳥取県.txt formatted/鳥取県.xls"
+ruby format.rb output/島根県.txt formatted/島根県.xls
+echo "島根県.txt formatted/島根県.xls"
+ruby format.rb output/岡山県.txt formatted/岡山県.xls
+echo "岡山県.txt formatted/岡山県.xls"
+ruby format.rb output/広島県.txt formatted/広島県.xls
+echo "広島県.txt formatted/広島県.xls"
+ruby format.rb output/山口県.txt formatted/山口県.xls
+echo "山口県.txt formatted/山口県.xls"
+ruby format.rb output/徳島県.txt formatted/徳島県.xls
+echo "徳島県.txt formatted/徳島県.xls"
+ruby format.rb output/香川県.txt formatted/香川県.xls
+echo "香川県.txt formatted/香川県.xls"
+ruby format.rb output/愛媛県.txt formatted/愛媛県.xls
+echo "愛媛県.txt formatted/愛媛県.xls"
+ruby format.rb output/高知県.txt formatted/高知県.xls
+echo "高知県.txt formatted/高知県.xls"
+ruby format.rb output/福岡県.txt formatted/福岡県.xls
+echo "福岡県.txt formatted/福岡県.xls"
+ruby format.rb output/佐賀県.txt formatted/佐賀県.xls
+echo "佐賀県.txt formatted/佐賀県.xls"
+ruby format.rb output/長崎県.txt formatted/長崎県.xls
+echo "長崎県.txt formatted/長崎県.xls"
+ruby format.rb output/熊本県.txt formatted/熊本県.xls
+echo "熊本県.txt formatted/熊本県.xls"
+ruby format.rb output/大分県.txt formatted/大分県.xls
+echo "大分県.txt formatted/大分県.xls"
+ruby format.rb output/宮崎県.txt formatted/宮崎県.xls
+echo "宮崎県.txt formatted/宮崎県.xls"
+ruby format.rb output/鹿児島県.txt formatted/鹿児島県.xls
+echo "鹿児島県.txt formatted/鹿児島県.xls"
+ruby format.rb output/沖縄県.txt formatted/沖縄県.xls
+echo "沖縄県.txt formatted/沖縄県.xls"
